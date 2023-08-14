b484f3103f519f45cd0d4539efa15ff1f9f3cd63
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-14 04:08:31 +0200

    Add http fetch

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,49 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GifsData, GifsResponse } from '../interfaces/gifs.interfaces';
+
+const GIPHY_API_KEY = 'uaWMEr5jFUS7LD0Aek6kMmZUNFU6t30D';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
-  private _tagsHistory: string[] = [];
+  public gifsList: GifsData[] = [];
 
-  constructor() {}
+  private _tagsHistory: string[] = [];
+  private apiKey: string = GIPHY_API_KEY;
+  private apiEndPoint: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string) {
+  private organizeHistory(tag: string) {
+    tag = tag.toLocaleLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', 10)
+      .set('offset', 0);
+
+    this.http
+      .get<GifsResponse>(`${this.apiEndPoint}/search`, { params })
+      .subscribe((response) => {
+        this.gifsList = response.data;
+        console.log(this.gifsList);
+      });
   }
 }
