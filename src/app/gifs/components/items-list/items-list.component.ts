9687bfc5fb1d371ebe656309651e5eec277e43f9
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-14 04:09:12 +0200

    Show list of gifs in gallery

--- src/app/gifs/components/items-list/items-list.component.ts
+++ src/app/gifs/components/items-list/items-list.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GifsData } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-list',
   templateUrl: './items-list.component.html',
 })
 export class GifsListComponent {
-  constructor() {}
+  @Input()
+  public gifs: GifsData[] = [];
 }
